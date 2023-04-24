66f9d686c99b20ffca13efa6d331078d2dc7b4f9
Benjamin Sherman <benjamin@holyarmy.org>
2023-04-23 19:11:13 -0500

    feat: add jq, tmux, yq

--- build.sh
+++ build.sh
@@ -8,9 +8,13 @@ dnf install -y \
   direnv \
   gh \
   hugo \
+  jq \
   pre-commit \
   python3-pip \
-  python3-virtualenv
+  python3-virtualenv \
+  tmux
 dnf update -y
+
+pip3 install yq
 
 dnf clean all

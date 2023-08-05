d4a5031cdf52a9d6513c2291a62971e2e7198dc7
Benjamin Sherman <benjamin@holyarmy.org>
2023-08-04 19:46:39 -0500

    fix: add ms vs code but actually do install

--- build.sh
+++ build.sh
@@ -42,6 +42,6 @@ dnf install -y \
 # install microsoft VS Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf install code
+dnf install -y code
 
 dnf clean all
